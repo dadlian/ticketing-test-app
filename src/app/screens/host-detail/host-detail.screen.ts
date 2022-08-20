a744298fe58cde8d1afd2a2a6ab26ee2c0bbe7ee
dadlian <sveninem@gmail.com>
2022-08-20 18:23:35 +0100

    Added Host Administrator Management

--- src/app/screens/host-detail/host-detail.screen.ts
+++ src/app/screens/host-detail/host-detail.screen.ts
@@ -1,27 +1,38 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Event, Advertisement, Host, TickeTing } from '@ticketing/angular';
-import { SessionManager } from '../../services/session.manager';
+import { Account, Event, Advertisement, Host, TickeTing } from '@ticketing/angular';
 import { EventManager } from '../../services/event.manager';
 import { AdManager } from '../../services/ad.manager';
+import { SessionManager } from '../../services/session.manager';
 
 @Component({
   templateUrl: './host-detail.screen.html',
   styleUrls:['./host-detail.screen.scss']
 })
 export class HostDetailScreen{
+  public activeUser: Account;
   public host: Host;
   public filter: string;
   public timestamp: number;
+  public administrators: Array<Account>;
+  public newAdmin: string;
+  public message: string;
+  public loading: boolean;
 
-  constructor(private _activatedRoute: ActivatedRoute, private _ticketing: TickeTing, private _router: Router,
-              private _sessionManager: SessionManager, private _eventManager: EventManager,
-              private _adManager: AdManager){
+  constructor(private _activatedRoute: ActivatedRoute, private _ticketing: TickeTing,
+              private _router: Router, private _eventManager: EventManager,
+              private _adManager: AdManager, private _sessionManager: SessionManager){
+    this.activeUser = null;
     this.host = null
     this.filter = ""
+    this.administrators = []
+    this.newAdmin = ""
+    this.message = ""
+    this.loading = false
   }
 
   ngOnInit(){
+    this.activeUser = this._sessionManager.getActiveSession().account
     this.filter = this._activatedRoute.snapshot.params.tab
 
     let hostURI = atob(this._activatedRoute.snapshot.params.host)
@@ -29,6 +40,10 @@ export class HostDetailScreen{
       this.host = host;
       this._eventManager.setActiveHost(host)
       this.timestamp = Date.now()
+
+      this.host.administrators.then(administrators => {
+        this.administrators = administrators
+      })
     })
   }
 
@@ -57,5 +72,35 @@ export class HostDetailScreen{
 
   submitAd(advertisement: Advertisement){
     advertisement.submit()
+  }
+
+  updateCandidate(event){
+    this.newAdmin = event.target.value
+  }
+
+  addAdministrator(){
+    if(this.newAdmin && !this.loading){
+      this.loading = true;
+      this.message = ""
+
+      this._ticketing.account.findUser(this.newAdmin).then(account=> {
+        this.host.addAdministrator(account).then(administrators => {
+          this.message = "Administrator added successfully"
+          this.administrators = administrators
+          this.newAdmin = ""
+        })
+      }).catch(error => {
+        this.message = "There is no user matching your search"
+      }).finally(() => {
+        this.loading = false;
+      })
+    }
+  }
+
+  removeAdministrator(administrator){
+    this.host.removeAdministrator(administrator).then(administrators => {
+      this.message = "Administrator removed successfully"
+      this.administrators = administrators
+    })
   }
 }
